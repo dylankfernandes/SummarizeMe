e746851b2fae2b81b842187f7be6128b342aa351
Dylan Fernandes <dylan.fernandes00@gmail.com>
2020-03-08 18:00:43 -0400

    finished summarization app

--- server.py
+++ server.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from gensim.summarization.summarizer import summarize
+import spacy
 
 app = Flask(__name__)
 
-@app.route('/hello/<user>')
-def index(user):
-  return render_template('index.html', name=user)
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/summarize', methods=["POST"])
+def get_summary():
+  if request.method == "POST":
+    submission = request.form['submission']
+    summary = summarize(submission).split('.')
+    
+    nlp = spacy.load("en_core_web_sm")
+    document = nlp(submission)
+    
+    people = []
+    for entity in document.ents:
+      print(entity.label)
+      if entity.label == 380:
+        people.append(entity.text)
+    
+    return render_template('summary.html', 
+                           summary=summary,
+                           people=set(people))
 
 if __name__ == '__main__':
   app.run(debug=True)
